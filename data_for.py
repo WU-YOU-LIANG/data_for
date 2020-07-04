3435ea2f940065b433c920144c68608c638bef30
WU-YOU-LIANg <82502290abc@gmail.com>
2020-07-04 22:08:27 +0800

    data_for_diff

--- data_for.py
+++ data_for.py
@@ -7,13 +7,55 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了,總共有', count, '筆資料')
-print(len(data))
-print(data[0])
-print('-------------')
-print(data[1])
 
-sum_len = 0 
+wc = {}
 for d in data:
-	sum_len = sum_len + len(d)
-print('平均資料的長度為: ', sum_len / len(data))
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
 
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+print(wc['a'])
+
+while True:
+	word = input('你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現的次數為', wc[word])
+	else:
+		print('這個字不存在')
+print('感謝您使用本查詢')
+
+
+
+
+
+
+
+
+
+
+# print(len(data))
+# print(data[0])
+# print('-------------')
+# print(data[1])
+
+# sum_len = 0 
+# for d in data:
+# 	sum_len = sum_len + len(d)
+# print('平均資料的長度為: ', sum_len / len(data))
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆資料長度小於100')
+
+	
